31bfb5f0c1905936af242e7bfdd27d2420b34e9e
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-16 10:20:46 +0700

    tipe data | string

--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main(){
-	// TODO: BOOLEAN
-	fmt.Println("Benar = ", true)
-	fmt.Println("Salah = ", false)
+	// TODO: STRING
+	fmt.Println("ACEP NURMAN SIDIK")
+
+	// TODO: function untuk string
+	// utk mengitung jumlah string
+	fmt.Println(len("ACEP"))
+	// mengambil karakter pada posisi yang ditentukan
+	fmt.Println("ACEP"[0])
+	fmt.Println("ACEP"[1])
 }
