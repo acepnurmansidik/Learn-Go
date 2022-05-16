6a9d081c69425d98f29a43184bb01f06815e0dcd
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-16 10:12:11 +0700

    tipe data | boolean

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello world")
-	// TODO: NUMBER
-	fmt.Println("bilangan koma =", 3.5)
+	// TODO: BOOLEAN
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
 }
