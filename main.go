d23e73e024aead06294c77c2b46d94b62c293abd
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-16 14:22:06 +0700

    tpe declaration

--- main.go
+++ main.go
@@ -3,21 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	// TODO: KONVERSI TIPE DATA
+	// TODO: TYPE DECLARATION
 
-	var nilai32 int32 = 128
-	var nilai64 int64 = int64(nilai32)
-	var nilai8 int8 = int8(nilai32)
+	type noKTP string
 
-	fmt.Println(nilai32)
-	fmt.Println(nilai64)
-	fmt.Println(nilai8)
-
-	name := "acep"
-	a := string(name[0])
-
-	fmt.Println(name)
-	fmt.Println(a)
+	var noKTPAcep noKTP = "54546466"
+	fmt.Println(noKTPAcep)
 
 
 }
