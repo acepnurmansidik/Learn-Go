d1cf8c9dd6d708dadf8f474a26e19621059bad6f
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-16 14:44:13 +0700

    operasi mtk

--- main.go
+++ main.go
@@ -3,12 +3,46 @@ package main
 import "fmt"
 
 func main(){
-	// TODO: TYPE DECLARATION
+	// TODO: OERASI MATEMATIKA
+	penjumlahan := 10 + 10
+	pengurangan := 10 - 10
+	perkalian := 10 * 10
+	pembagian := 10 / 10
+	modulus := 10 % 3
 
-	type noKTP string
+	fmt.Println(penjumlahan)
+	fmt.Println(pengurangan)
+	fmt.Println(perkalian)
+	fmt.Println(pembagian)
+	fmt.Println(modulus)
+	
+	fmt.Println("Augmanted assigments ##################################")
 
-	var noKTPAcep noKTP = "54546466"
-	fmt.Println(noKTPAcep)
+	// Augmanted assigments
+	i := 10
+	i += 10
+	fmt.Println(i)
+	i -= 8
+	fmt.Println(i)
+	i *= 10
+	fmt.Println(i)
+	i /= 10
+	fmt.Println(i)
+	i %= 10
+	fmt.Println(i)
 
+	fmt.Println("Unary operator ##################################")
+
+	// Unary operator
+	i++ // i = i + 1
+	fmt.Println(i)
+	i--
+	fmt.Println(i)
+	negative := -100
+	fmt.Println(negative)
+	positive := +100
+	fmt.Println(positive)
+	married := true
+	fmt.Println(!married)
 
 }
