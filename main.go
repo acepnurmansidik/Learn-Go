ac598a75fb48364edd0d34681feef67fcd9dd7ea
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-17 14:42:02 +0700

    percabangan swith expression

--- main.go
+++ main.go
@@ -5,12 +5,32 @@ import "fmt"
 func main(){
 	// TODO: PNEGKONDISIAN /PERCABANGAN
 
-	// if else
 	name := "acep"
-	if name == "nurman"{
-		fmt.Println("Hai acep")
-	}else{
-		fmt.Println("Kenalin ")
+
+	switch name{
+	case "Nurman":
+		fmt.Println("Hello nurmnan")
+	case "Sidik":
+		fmt.Println("Hello sidik")
+	default:
+		fmt.Println("kenalan yu")
+	}
+	// short statement
+	switch length := len(name); length > 5{
+	case true:
+		fmt.Println("Too long")
+	case false:
+		fmt.Println("Too short")
+	}
+
+	// swith with expression
+	switch {
+	case len(name) > 10:
+		fmt.Println("nama terlalu panjang")
+	case len(name) > 5:
+		fmt.Println("nama lumayan panjang")
+	default:
+		fmt.Println("nama terlalu pendek")
 	}
 
 
