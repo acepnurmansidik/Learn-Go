b58152fa2efad9ca851f8a9be4542f59f1af1584
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-18 07:46:02 +0700

    Break & COntinue

--- main.go
+++ main.go
@@ -5,38 +5,23 @@ import (
 )
 
 func main(){
-	// TODO: PERULANGAN
+	// TODO: BREAK & CONTINUE
 
-	// for biasa
-	fmt.Println("####### Perulangan For biasa ####")
-	counter := 1
-	for counter <= 3{
-		fmt.Println("Perulangan ke", counter)
-		counter++
+	// break
+	for i := 0; i < 10; i++{
+		if i == 5{
+			break
+		}
+		fmt.Println("Perulangan ke",i)
 	}
 
-	// for dengan statement
+	// continue
 	fmt.Println("")
-	fmt.Println("####### Perulangan for dengan statement ####")
-	for counter1 := 1; counter1 <= 3; counter1++{
-		fmt.Println("Perulangan ke", counter1)
-	}
-
-	// for range
-	fmt.Println("")
-	fmt.Println("####### Perulangan for dengan statement ####")
-	// array / slice
-	// tanda underscore berfungsi jika key nya tidak digunakan
-	cars := []string{"Tesla", "Honda", "Toyoya"}
-	for _, car := range cars {
-		fmt.Println(car)
-	}
-	fmt.Println("")
-	person := make(map[string]string)
-	person["name"] = "acep"
-	person["address"] = "garut"
-
-	for key, value := range person{
-		fmt.Println(key,"=", value)
+	fmt.Println("CONTINUE #########################")
+	for i := 0; i < 10; i++{
+		if i % 2 == 0{
+			continue
+		}
+		fmt.Println("Perulangan ke", i)
 	}
 }
