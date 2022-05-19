2977ac4e39dc1368e3ca8258879a01fb01ae458f
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-19 08:14:50 +0700

    function as parameter

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main(){
-	// TODO: FUNCTION AS VALUE
+	// TODO: FUNCTION AS PARAMETER
 
-	result := getGoodBye
-	fmt.Println(result("acep"))
+	sayHelloWithFilter("Anjing",spamFilter)
+
 }
 
-func getGoodBye(name string)string{
-	return "Good bye "+name 
+// jika terlalu aonajng bisa menggunakan alias
+type Filter func(string) string
+
+func sayHelloWithFilter(name string, filter Filter){
+	nameFilter := filter(name)
+	fmt.Println("Hello",nameFilter)
+}
+
+func spamFilter(name string)string{
+	if (name == "Anjing"){
+		return "..."
+	}else{
+		return name
+	}
 }
