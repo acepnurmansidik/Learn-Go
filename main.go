068fd725112adf187b80b7a853db1336bfadd538
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-19 11:07:39 +0700

    closure

--- main.go
+++ main.go
@@ -3,41 +3,19 @@ package main
 import "fmt"
 
 func main(){
-	// TODO: RECURSIVE FUNCTION
+	// TODO: CLOSURE
+	name := "acep"
+	counter := 0
 
-	// menggunakan for loops
-	blocklist := func (value int)int  {
-		result := 1
-		for i := value; i > 0; i--{
-			result *= i
-		}
-		return result
+	increment := func ()  {
+		name := "nurman"
+		fmt.Println("increment")
+		fmt.Println(name)
+		counter++
 	}
 
-	fmt.Println("without recursive")
-	fmt.Println(blocklist(5))
-
-	// Recursive
-	fmt.Println("recursive")
-	fmt.Println(factorialRecursive(5))
+	increment()
+	fmt.Println(name)
+	fmt.Println(counter)
 }
 
-// jika terlalu aonajng bisa menggunakan alias
-type Blocklist func(string) bool
-
-func registerUser(name string, blocklist Blocklist){
-	if blocklist(name) {
-		fmt.Println("Yo're blocked",name)
-	}else {
-		fmt.Println("Welcome",name)
-	}
-}
-
-	func factorialRecursive(value int)int  {
-		if(value == 1){
-			return 1
-		}else {
-			return value * factorialRecursive(value-1)
-		}
-		
-	}
