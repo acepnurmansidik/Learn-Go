0f3ffb49a8e7a97bd096e7f22ac8a210b98577cb
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-19 21:20:37 +0700

    struct

--- main.go
+++ main.go
@@ -4,60 +4,31 @@ import (
 	"fmt"
 )
 
+type Customer struct{
+	Name string
+	Address string
+	Age int
+}
+
 func main(){
-	// TODO: DEFER, PANIC & RECOVER
-
-	// defer
-	// akan menjalankan function ketika sudah selesai programnya meskipun ada error
-	logging := func ()  {
-		fmt.Println("Selesai memangil function")
-	}
-
-	runApplication := func(value int){
-		defer logging()
-		fmt.Println("Run Application")
-		result := 10 /value
-		fmt.Println(result)
-	}
-
-	fmt.Println("## D E F E R ##################")
-	runApplication(10)
+	// TODO: STRUCT
 	
-	// Panic
-	endApp := func ()  {
-		fmt.Println("Aplikais selesai")
+	// kumpulan dari beberapa data
+	var person Customer
+	person.Name = "acep"
+	person.Address = "Garut"
+	person.Age = 23
+
+	fmt.Println(person)
+
+	// struct literal
+	people := Customer{
+		Name: "nurman",
+		Address: "garut",
+		Age: 23,
 	}
 
-	rundApp := func (error bool)  {
-		defer endApp()
-		if error {
-			panic("APLIKAIS ERROR!!!")
-		}
-		fmt.Println("Aplikasi berjalan")
-	}
-	fmt.Println("")
-	fmt.Println("## P A N I C ##################")
-	rundApp(false)
+	fmt.Println(people)
 
-	fmt.Println("")
-	fmt.Println("## R E C O V E R ##################")
-	// recover akan menangkap response dari panik
-	// recover hanya disimpan di defer
-	// recover akan terus berjalan meskipun ada error
-	akhirApp := func ()  {
-		message := recover()
-		if message != nil{
-			fmt.Println("Error denga message: ",message)
-		}
-		fmt.Println("Aplikasi selesai")
-	}
-	App := func (error bool)  {
-		defer akhirApp()
-		if error {
-			panic("APLIKASI ERROR!")
-		}
-	}
-
-	App(true)
 }
 
