38fd578d426fc304792b6c6bf53a4dedbac05779
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-20 10:54:50 +0700

    interface kosong

--- main.go
+++ main.go
@@ -4,39 +4,22 @@ import (
 	"fmt"
 )
 
-type Person struct{
-	Name string
+func Ups(value int) interface{} {
+	if value == 1 {
+		return 1
+	} else if value == 2 {
+		return true
+	} else {
+		return "Ups"
+	}
 }
 
-type Animal struct{
-	Name string
-}
-
-type HasName interface{
-	GetName() string
-}
-
-func sayHello(name HasName){
-	fmt.Println("Hello",name.GetName())
-}
-
-func (person Person) GetName()string{
-	return person.Name
-}
-
-func (animal Animal) GetName() string{
-	return animal.Name
-}
-
-
-func main(){
+func main() {
 	// TODO: INTERFACE
 
 	// interface biasa digunakan untuk function2 yang general
-	person := Person{Name: "acep"}
-	sayHello(person)
-	
-	cat := Animal{Name: "Kucing"}
-	sayHello(cat)
-}
 
+	// interface kosong
+	var data interface{} = Ups(5)
+	fmt.Println(data)
+}
