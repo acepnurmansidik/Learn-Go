9b431ca6a7234b9ca958cd29182426ac585d44de
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-21 17:22:51 +0700

    pointer di mehod

--- main.go
+++ main.go
@@ -4,45 +4,19 @@ import (
 	"fmt"
 )
 
-type Address struct{
-	City, Province, Country string
+type Man struct{
+	Name string
 }
 
-func changeCountryToIndonesia(address *Address){
-	address.Country = "Indonesia"
+func (man *Man) Married(){
+	man.Name = "Mr. " + man.Name
 }
 
 func main() {
-	// TODO: POINTER
+	// TODO: POINTER DI METHOD
 
-	// pass by value, jika di asignment ke variabel lain valuenya kan di copy
-	address1 := Address{"Subang", "West Java", "Indonesia"}
-	address2 := &address1
-	address3 := &address1
-
-	address2.City = "Garut"
-
-	// jka ditambahkan pointer seolah akan membuat memory baru
-	// address2 = &Address{"Subang", "West Java", "Indonesia"}
-
-	// jika ditambahkan bintang di depannya maka akan memaksa semua variabel ke address 2
-	*address2 = Address{"Subang", "West Java", "Indonesia"}
-
-	fmt.Println(address1)
-	fmt.Println(address2)
-	fmt.Println(address3)
-
-	// pointer new, akan mengembalikan data kosong/tidak ada data awal
-	address4 := new(Address)
-	address4.City = "Surabaya"
-	fmt.Println(address4)
-
-	// POINTER DI FUNCTION
-
-	alamat := Address{
-		City: "Malang",
-		Province: "Jawa Timur",
-	}
-	changeCountryToIndonesia(&alamat)
-	fmt.Println(alamat)
+	// jika buat struct function/methode disarankan menggunakan pointer
+	acep := Man{Name: "acep"}
+	acep.Married()
+	fmt.Println(acep.Name)
 }
