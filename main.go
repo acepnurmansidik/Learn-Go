58478b555d0ad998b1fff72794f182111b9108a2
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-23 13:18:23 +0700

    package sort

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
-	"container/ring"
 	"fmt"
-	"strconv"
+	"sort"
 )
 
+type User struct{
+	Name string
+	Age int
+}
+
+type UserSlice []User
+
+func (value UserSlice) Len() int {
+	return len(value)
+}
+
+func (value UserSlice) Less(i, j int) bool{
+	return value[i].Age < value[j].Age 
+}
+
+func (value UserSlice) Swap(i, j int){
+	value[i], value[j] = value[j], value[i]
+}
+
 func main() {
-	// TODO: PACKAGE CONTAINER/RING
-
-	data := ring.New(5)
-
-	for i := 0; i < data.Len(); i++ {
-		data.Value = "Data " + strconv.FormatInt(int64(i), 10)
-		data = data.Next()
+	// TODO: PACKAGE SORT
+	users := []User{
+		{"Acep", 35},
+		{"Nurman", 21},
+		{"Sidik", 40},
 	}
 
-	data.Do(func(value interface{}) {
-		fmt.Println(value)
-	})
+	fmt.Println(users)
+	sort.Sort(UserSlice(users))
+	fmt.Println(users)
 }
