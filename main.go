d77bbafe9746c79730e9b33211db6d78f2f302b0
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-24 07:47:55 +0700

    package reflect | structTag

--- main.go
+++ main.go
@@ -6,14 +6,13 @@ import (
 )
 
 type Sample struct {
-	Name string
+	Name string `required:"true" max:"10"`
 }
 
 func main() {
-	// TODO: PACKAGE REFLECT
+	// TODO: PACKAGE REFLECT | StructTag
 
 	sample := Sample{"Acep"}
 	sampleType := reflect.TypeOf(sample)
-	fmt.Println(sampleType.NumField())
-	fmt.Println(sampleType.Field(0).Name)
+	fmt.Println(sampleType.Field(0).Tag.Get("max"))
 }
